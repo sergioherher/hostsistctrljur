84aacfc183faa29931e85e4c35a97848d7f716e1
sergioherher <sergiovm@facilposcorreo@gmail.com>
2019-05-13 02:43:08 -0400

    Commit inicial

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,183 @@
+@extends('auth.app')
+
+@section('content')
+<!-- begin:: Page -->
+<div class="kt-grid kt-grid--ver kt-grid--root">
+    <div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v4 kt-login--signin" id="kt_login">
+        <div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" style="background-image: url('{{asset('theme/assets/media/bg/bg-2.jpg')}}');">
+            <div class="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
+                <div class="kt-login__container">
+                    <div class="kt-login__logo">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="96px" height="96px" viewBox="0 0 24 24" version="1.1">
+                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <rect id="bound" x="0" y="0" width="24" height="24"/>
+                                    <path d="M13.6855025,18.7082217 C15.9113859,17.8189707 18.682885,17.2495635 22,17 C22,16.9325178 22,13.1012863 22,5.50630526 L21.9999762,5.50630526 C21.9999762,5.23017604 21.7761292,5.00632908 21.5,5.00632908 C21.4957817,5.00632908 21.4915635,5.00638247 21.4873465,5.00648922 C18.658231,5.07811173 15.8291155,5.74261533 13,7 C13,7.04449645 13,10.79246 13,18.2438906 L12.9999854,18.2438906 C12.9999854,18.520041 13.2238496,18.7439052 13.5,18.7439052 C13.5635398,18.7439052 13.6264972,18.7317946 13.6855025,18.7082217 Z" id="Combined-Shape" fill="#5d78ff"/>
+                                    <path d="M10.3144829,18.7082217 C8.08859955,17.8189707 5.31710038,17.2495635 1.99998542,17 C1.99998542,16.9325178 1.99998542,13.1012863 1.99998542,5.50630526 L2.00000925,5.50630526 C2.00000925,5.23017604 2.22385621,5.00632908 2.49998542,5.00632908 C2.50420375,5.00632908 2.5084219,5.00638247 2.51263888,5.00648922 C5.34175439,5.07811173 8.17086991,5.74261533 10.9999854,7 C10.9999854,7.04449645 10.9999854,10.79246 10.9999854,18.2438906 L11,18.2438906 C11,18.520041 10.7761358,18.7439052 10.4999854,18.7439052 C10.4364457,18.7439052 10.3734882,18.7317946 10.3144829,18.7082217 Z" id="Path-41-Copy" fill="#5d78ff" opacity="0.3"/>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="kt-login__signin">
+                        <div class="kt-login__head">
+                            <h3 class="kt-login__title">Sistema de Control de Hipotecas</h3>
+                        </div>
+                        <form class="kt-form" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+                            </div>
+                            <div class="input-group">
+                                <input class="form-control" type="password" placeholder="Password" name="password">
+                            </div>
+                            <div class="row kt-login__extra">
+                                <div class="col">
+                                    <label class="kt-checkbox">
+                                        <input type="checkbox" name="remember"> Recordarme
+                                        <span></span>
+                                    </label>
+                                </div>
+                                <div class="col kt-align-right">
+                                    <a href="javascript:;" id="kt_login_forgot" class="kt-login__link">¿Olvidó su password?</a>
+                                </div>
+                            </div>
+                            <div class="kt-login__actions">
+                                <button id="kt_login_signin_submit" class="btn btn-brand btn-pill kt-login__btn-primary">Ingresar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="kt-login__signup">
+                        <div class="kt-login__head">
+                            <h3 class="kt-login__title">Registrarse</h3>
+                            <div class="kt-login__desc">Ingrese los datos para crear su cuenta:</div>
+                        </div>
+                        <form class="kt-form" method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Nombre" name="fullname">
+                            </div>
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Email" name="email" autocomplete="off">
+                            </div>
+                            <div class="input-group">
+                                <input class="form-control" type="password" placeholder="Password" name="password">
+                            </div>
+                            <div class="input-group">
+                                <input class="form-control" type="password" placeholder="Confirme su password" name="rpassword">
+                            </div>
+                            <div class="row kt-login__extra">
+                                <div class="col kt-align-left">
+                                    <label class="kt-checkbox">
+                                        <input type="checkbox" name="agree">Estoy de acuerdo con los <a href="#" class="kt-link kt-login__link kt-font-bold">términos y condiciones de esta página</a>.
+                                        <span></span>
+                                    </label>
+                                    <span class="form-text text-muted"></span>
+                                </div>
+                            </div>
+                            <div class="kt-login__actions">
+                                <button id="kt_login_signup_submit" class="btn btn-brand btn-pill kt-login__btn-primary">Sign Up</button>&nbsp;&nbsp;
+                                <button id="kt_login_signup_cancel" class="btn btn-secondary btn-pill kt-login__btn-secondary">Cancel</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="kt-login__forgot">
+                        <div class="kt-login__head">
+                            <h3 class="kt-login__title">¿Olvidó su password?</h3>
+                            <div class="kt-login__desc">Ingrese su email para resetearlo:</div>
+                        </div>
+                        <form class="kt-form" method="POST" action="{{ route('password.email') }}">
+                            @csrf
+                            <div class="input-group">
+                                <input class="form-control" type="text" placeholder="Email" name="email" id="kt_email" autocomplete="off">
+                            </div>
+                            <div class="kt-login__actions">
+                                <button id="kt_login_forgot_submit" class="btn btn-brand btn-pill kt-login__btn-primary">Solicitar</button>&nbsp;&nbsp;
+                                <button id="kt_login_forgot_cancel" class="btn btn-secondary btn-pill kt-login__btn-secondary">Cancelar</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="kt-login__account">
+                        <span class="kt-login__account-msg">
+                            ¿Aún no tiene una cuenta?
+                        </span>
+                        &nbsp;&nbsp;
+                        <a href="javascript:;" id="kt_login_signup" class="kt-login__account-link">¡Creela!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end:: Page -->
+
+@endsection
+
+<div class="container" style="display: none;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
